8b79242c4b640bf126413b023f19f38b1a83a078
Krailo Anatoliy <ay.krailo@gmail.com>
2021-01-25 17:22:42 +0200

    250121 v5

---
+++ src/main/java/ua/com/foxminded/krailo/university/dao/VocationDao.java
@@ -0,0 +1,62 @@
+package ua.com.foxminded.krailo.university.dao;
+
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Component;
+
+import ua.com.foxminded.krailo.university.model.Vocation;
+
+@Component
+public class VocationDao {
+
+    private static final String SQL_SELECT_ALL = "SELECT * FROM vocations ORDER BY id";
+    private static final String SQL_SELECT_BY_ID = "SELECT * FROM vocations WHERE id = ?";
+    private static final String SQL_UPDATE_BY_ID = "UPDATE vocations SET kind = ?, applying_date = ?, start_date = ?, end_date = ?, teacher_id = ? WHERE id = ?";
+    private static final String SQL_DELETE_BY_ID = "DELETE FROM vocations WHERE id = ?";
+    private static final String SQL_INSERT_VOCATION = "INSERT INTO vocations (kind, applying_date, start_date, end_date, teacher_id) VALUES (?, ?, ?, ?, ?)";
+
+    private JdbcTemplate jdbcTemplate;
+    private RowMapper<Vocation> vocationRowMapper;
+
+    public VocationDao(JdbcTemplate jdbcTemplate, RowMapper<Vocation> vocationRowMapper) {
+	this.jdbcTemplate = jdbcTemplate;
+	this.vocationRowMapper = vocationRowMapper;
+    }
+
+    public Vocation findById(int id) {
+	return jdbcTemplate.queryForObject(SQL_SELECT_BY_ID, vocationRowMapper, id);
+    }
+
+    public List<Vocation> findAll() {
+	return jdbcTemplate.query(SQL_SELECT_ALL, vocationRowMapper);
+    }
+
+    public void create(Vocation vocation) {
+	KeyHolder keyHolder = new GeneratedKeyHolder();
+	jdbcTemplate.update(connection -> {
+	    PreparedStatement ps = connection.prepareStatement(SQL_INSERT_VOCATION, new String[] { "id" });
+	    ps.setString(1, vocation.getKind());
+	    ps.setDate(2, Date.valueOf(vocation.getApplyingDate()));
+	    ps.setDate(3, Date.valueOf(vocation.getStart()));
+	    ps.setDate(4, Date.valueOf(vocation.getEnd()));
+	    ps.setInt(5, vocation.getTeacher().getId());
+	    return ps;
+	}, keyHolder);
+	vocation.setId(keyHolder.getKey().intValue());
+    }
+
+    public void update(Vocation vocation) {
+	jdbcTemplate.update(SQL_UPDATE_BY_ID, vocation.getKind(), Date.valueOf(vocation.getApplyingDate()), Date.valueOf(vocation.getStart()), Date.valueOf(vocation.getEnd()), vocation.getTeacher().getId()), vocation.getId());
+    }
+
+    public void deleteById(int id) {
+	jdbcTemplate.update(SQL_DELETE_BY_ID, id);
+    }
+
+}
---
+++ src/main/java/ua/com/foxminded/krailo/university/mapper/VocationRowMapper.java
@@ -0,0 +1,34 @@
+package ua.com.foxminded.krailo.university.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import ua.com.foxminded.krailo.university.dao.TeacherDao;
+import ua.com.foxminded.krailo.university.model.Vocation;
+
+@Component
+public class VocationRowMapper implements RowMapper<Vocation> {
+
+    private TeacherDao teacherDao;
+
+    public VocationRowMapper(TeacherDao teacherDao) {
+	this.teacherDao = teacherDao;
+    }
+
+    @Override
+    public Vocation mapRow(ResultSet rs, int rowNum) throws SQLException {
+	Vocation vocation = new Vocation();
+	vocation.setId(rs.getInt("id"));
+	vocation.setKind(rs.getString("kind"));
+	vocation.setApplyingDate(rs.getObject("applying_date", LocalDate.class));
+	vocation.setStart(rs.getObject("start_date", LocalDate.class));
+	vocation.setEnd(rs.getObject("end_date", LocalDate.class));
+	vocation.setTeacher(teacherDao.findById(vocation.getTeacher().getId()));
+	return vocation;
+    }
+
+}
