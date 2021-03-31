e401965b3cb8fec77601129dfb073feb14ed10fa
Krailo Anatoliy <ay.krailo@gmail.com>
2021-03-31 06:14:54 +0300

    task 14 march20

--- src/main/java/ua/com/foxminded/krailo/university/controllers/SpecialtyController.java
+++ src/main/java/ua/com/foxminded/krailo/university/controllers/SpecialtyController.java
@@ -7,17 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import ua.com.foxminded.krailo.university.model.Faculty;
 import ua.com.foxminded.krailo.university.model.Specialty;
-import ua.com.foxminded.krailo.university.model.Teacher;
 import ua.com.foxminded.krailo.university.model.Year;
-import ua.com.foxminded.krailo.university.service.FacultyService;
 import ua.com.foxminded.krailo.university.service.SpecialtyService;
-import ua.com.foxminded.krailo.university.service.TeacherService;
 import ua.com.foxminded.krailo.university.service.YearService;
 
 @Controller
@@ -27,7 +21,7 @@ public class SpecialtyController {
     @Autowired
     YearService yearService;
     @Autowired
-    SpecialtyService speciltyService;
+    SpecialtyService specialtyService;
     
 
     @GetMapping()
@@ -37,14 +31,14 @@ public class SpecialtyController {
     
     @GetMapping("/all")
     public String getAllSpecialties(Model model) {
-	List<Specialty> specialties = speciltyService.getAll();
+	List<Specialty> specialties = specialtyService.getAll();
 	model.addAttribute("specialties", specialties);
 	return "specialties/specialtiesAll";
     }
     
     @GetMapping("/findSpecialtyById/{id}")
     public String getViewSpecialty (@PathVariable("id") int id, Model model) {
-	Specialty specialty = speciltyService.getById(id);
+	Specialty specialty = specialtyService.getById(id);
 	List<Year> years = yearService.getBySpecialtyId(specialty.getId());
 	specialty.setYears(years);
 	model.addAttribute("specialty", specialty);
