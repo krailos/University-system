4dea59acfb8ac48061c5b6ccbc00920939ac163c
Krailo Anatoliy <ay.krailo@gmail.com>
2021-04-01 02:00:29 +0300

    task14 march22

--- src/test/java/ua/com/foxminded/krailo/university/controllers/AudienceControllerTest.java
+++ src/test/java/ua/com/foxminded/krailo/university/controllers/AudienceControllerTest.java
@@ -1,8 +1,11 @@
 package ua.com.foxminded.krailo.university.controllers;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,7 +20,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import ua.com.foxminded.krailo.university.model.Audience;
 import ua.com.foxminded.krailo.university.service.AudienceService;
@@ -33,27 +35,25 @@ class AudienceControllerTest {
 
     @BeforeEach
     public void setUp() {
-	mockMvc = MockMvcBuilders.standaloneSetup(audienceController).build();
+	mockMvc = standaloneSetup(audienceController).build();
     }
 
     @Test
     void whenGetAllAudiences_thenAllAudiencesReturned() throws Exception {
 	List<Audience> expected = getAudiencesForTest();
 	when(audienceService.getAll()).thenReturn(expected);
-	MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/audiences/all");
-	ResultActions result = mockMvc.perform(request);
 
-	result.andExpect(view().name("audiences/audiencesAll")).andExpect(model().attribute("audiences", expected));
+	mockMvc.perform(get("/audiences/all")).andExpect(view().name("audiences/audiencesAll"))
+		.andExpect(status().isOk()).andExpect(model().attribute("audiences", expected));
     }
 
     @Test
     void givenAudienceId_whenGetAudience_thenGotAudience() throws Exception {
 	Audience expected = getAudiencesForTest().get(0);
 	when(audienceService.getById(1)).thenReturn(expected);
-	MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/audiences/findAudienceById/1");
-	ResultActions result = mockMvc.perform(request);
 
-	result.andExpect(view().name("audiences/audienceView")).andExpect(model().attribute("audience", expected));
+	mockMvc.perform(get("/audiences/findAudienceById/1")).andExpect(view().name("audiences/audienceView"))
+		.andExpect(model().attribute("audience", expected));
     }
 
     private List<Audience> getAudiencesForTest() {
