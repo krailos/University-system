c8ca47fb827cbe964ab7837e82fa16a007200c15
Krailo Anatoliy <ay.krailo@gmail.com>
2021-06-08 14:45:29 +0300

    fixed dependencies

--- src/main/java/ua/com/foxminded/krailo/university/config/WebConfig.java
+++ src/main/java/ua/com/foxminded/krailo/university/config/WebConfig.java
@@ -1,10 +1,18 @@
 package ua.com.foxminded.krailo.university.config;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -18,10 +26,15 @@ import ua.com.foxminded.krailo.university.formatters.GroupFormatter;
 import ua.com.foxminded.krailo.university.formatters.SubjectFormatter;
 import ua.com.foxminded.krailo.university.formatters.TeacherFormatter;
 
+@EnableSpringDataWebSupport
 @Configuration
 @ComponentScan("ua.com.foxminded.krailo.university")
+@PropertySource("classpath:config.properties")
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${spring.data.pageable.defaulPageSize}")
+    private int pagableDefaultPageSize;
 
     @Bean
     public SpringResourceTemplateResolver templateResolver(ApplicationContext applicationContext) {
@@ -63,7 +76,13 @@ public class WebConfig implements WebMvcConfigurer {
 	registry.addFormatter(new SubjectFormatter());
 	registry.addFormatter(new TeacherFormatter());
     }
-    
-  
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+	PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
+	resolver.setOneIndexedParameters(true);
+	resolver.setFallbackPageable(PageRequest.of(0, pagableDefaultPageSize));
+	resolvers.add(resolver);
+    }
 
 }
